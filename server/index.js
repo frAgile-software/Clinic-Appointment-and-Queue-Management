ab78fb9709097332e722460f540766e535aaade6
r-fletcher <fletcher.ryj@gmail.com>
2026-04-08 15:47:50 +0200

    implementation of initial code structure for server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+//example api implementation
+server.get("/api/hello", (req, res) => {
+    res.json({message : "Hello world!"});
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
