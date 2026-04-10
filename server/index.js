2f5d3f2f11962432cd4d2bd4d1adac24f16f86d0
r-fletcher <fletcher.ryj@gmail.com>
2026-04-10 15:40:49 +0200

    Update server/index.js (removed unnecessary cors fields)

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const server = express();
 
 server.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    origin: process.env.CLIENT_URL
 }));
 
 server.use(express.json());
