ac14f4daf3cd99ed13737e005e7cc3d830d0c4cd
Bruno Faria <brunofariav@gmail.com>
2026-04-12 15:25:02 +0200

    remove line 3 in in the test file

--- client/src/pages/AdminDashboard/AdminDashboard.test.js
+++ client/src/pages/AdminDashboard/AdminDashboard.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import AdminDashboard from './AdminDashboard';
-import { BrowserRouter } from 'react-router-dom'
+
 
 describe('AdminDashboard Component', () => {
 
