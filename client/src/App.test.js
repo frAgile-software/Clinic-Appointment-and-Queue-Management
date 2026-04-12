bd9bf59ad89569ef7d1cd179ea7f0948d796b7e0
HaydnRad <haydn.r.cooke@gmail.com>
2026-04-12 21:16:07 +0200

    Feat: slightly more useful App test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,6 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test('blank test', () => {
-  // render(<App />);
+test('app renders', () => {
+  render(<App />);
 });
