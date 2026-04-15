1bde0971cbc39680763557ccb607cbaba0d0effe
r-fletcher <fletcher.ryj@gmail.com>
2026-04-15 21:11:49 +0200

    Appointment schema implementation

--- server/src/database/models/Appointment.js
+++ server/src/database/models/Appointment.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const appointmentSchema = new Schema({
-    
+    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true},            // FK
+    Staff: {type: Schema.Types.ObjectId, ref: 'User', required: true},              // FK
+    Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true},           // FK
+    BookingDateTime: {type: Date, required: true},
+    Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}    // FK - reason changed to speciality 
+    // can add a comment field for patients to add more details?
 });
 
 export default mongoose.model('Appointment', appointmentSchema);
