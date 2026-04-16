e0c8227c14eb55a22f13c065b61a6640d8bf993c
Ryx13 <ryanmuzi13@gmail.com>
2026-04-16 22:15:29 +0200

    test: mock auth0 provider and update test assertions for landing and registration

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./database/dbConnect")
+const registerRoute = require("../routes/register");
+const userRoute = require("../routes/user");
+
 
 dbConnect();
 
@@ -11,6 +14,9 @@ server.use(cors({
 }));
 
 server.use(express.json());
+
+server.use("/api/register", registerRoute);
+server.use("/api/user", userRoute);
 
 //example api implementation
 server.get("/api/hello", (req, res) => {
