3a6db342201de32fc8404a28e7c71d31b158b139
Haydn Rad Cooke <haydn.r.cooke@gmail.com>
2026-04-16 19:00:35 +0200

    Update Speciality.js

--- server/src/database/models/Speciality.js
+++ server/src/database/models/Speciality.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const specialitySchema = new Schema({
-    Speciality: {type: Schema.Types.ObjectId, ref: 'Speciality', required: true}, // PK
     SpecialityName: { type: Schema.Types.String, ref: 'SpecialityName', required: true}
 });
 
