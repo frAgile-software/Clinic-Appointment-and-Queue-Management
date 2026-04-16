b42cf28a9505d1dea9fb76eecd42cf760ff5b8a9
21Ethan <bothaethan44@gmail.com>
2026-04-16 10:45:04 +0200

    staff speciality schema designed

--- server/src/database/models/StaffSpeciality.js
+++ server/src/database/models/StaffSpeciality.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const StaffSpeciality = new Schema({
-  
+  Staff:{ type: Schema.Types.ObjectId, ref: 'Staff' }, //FK
+  Speciality:{ type: Schema.Types.ObjectId, ref: 'Speciality' } //Fk
 });
 
 export default StaffSpeciality;
