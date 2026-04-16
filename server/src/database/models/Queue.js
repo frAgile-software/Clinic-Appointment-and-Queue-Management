cac2991d9423322b5045051528d35dbe9345a01d
21Ethan <bothaethan44@gmail.com>
2026-04-16 10:35:51 +0200

    queue schema designed

--- server/src/database/models/Queue.js
+++ server/src/database/models/Queue.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
-
-const Queue = new Schema({
-  
+                
+const QueueSchema = new Schema({
+     Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true}, //FK
+    Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}, //FK
+    Patient: {type: Schema.Types.ObjectId, ref: 'Patient', required: true}, //FK
+    Reason: {type: String, required: true},
+    EntryTime: {type: Date, default: Date.now}, //First in first out logic based on time entered
+    SequenceNumber: {type: Number, required: true}, //Auto-incremented number for queue order
+    //I assume we remove records as they are processed.
+    //If not, we would need a status field to indicate if the patient is waiting, in progress, or completed.
+    //And then clear the queue at the end of everyday? 
 });
 
-export default Queue;
+export default mongoose.model('Queue', QueueSchema);
