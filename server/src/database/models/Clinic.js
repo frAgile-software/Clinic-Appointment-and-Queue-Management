bf98df3485e5ab33813a98b5a23dcca16ae69105
Bruno Faria <brunofariav@gmail.com>
2026-04-16 15:28:30 +0200

    created clinic schema

--- server/src/database/models/Clinic.js
+++ server/src/database/models/Clinic.js
@@ -1,8 +1,43 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const Clinic = new Schema({
-  
+const clinicSchema = new Schema({
+  province: {
+    type: String,
+    required: true
+  },
+  physicalTown: {
+    type: String,
+    required: true
+  },
+  physicalSuburb: {
+    type: String,
+    required: true
+  },
+  physicalAddress: {
+    type: String,
+    required: true
+  },
+  practiceName: {
+    type: String,
+    required: true
+  },
+  practiceType: {
+    type: String,
+    required: true
+  },
+  practiceTypeDescription: {
+    type: String,
+    required: true
+  },
+  practiceNumber: {
+    type: String,
+    required: true
+  },
+  contactNumber: {
+    type: String,
+    required: true
+    }
 });
 
-export default Clinic;
+export default mongoose.model('Clinic', clinicSchema);
