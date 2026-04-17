3b0da5d93bb343171de2e1a7472311e5abbf60fe
r-fletcher <fletcher.ryj@gmail.com>
2026-04-17 20:39:13 +0200

    Created apiAuth file

---
+++ client/src/hooks/apiAuth.js
@@ -0,0 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+export const useApiAuth = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const apiFetch = useCallback(async (url, options = {}) => {
+        const token = getAccessTokenSilently({
+            authorizationParams: {
+                audience: `${process.env.REACT_CLIENT_SERVER_URL}`,
+            },
+        });
+
+        const response = await fetch(url, {
+            ...options,
+            headers: {
+                ...options.headers,
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        return response.json();
+    }, [getAccessTokenSilently]);
+
+    return {apiFetch};
+}
