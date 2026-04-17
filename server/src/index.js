431ad10058c179b4ef81d90f247bc784f412ef8a
r-fletcher <fletcher.ryj@gmail.com>
2026-04-17 21:37:18 +0200

    fix: changed require to import server/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dbConnect = require("./database/dbConnect")
 const registerRoute = require("../routes/register");
 const userRoute = require("../routes/user");
-const { requireAuth } = require('./middleware/auth');
+import { requireAuth } from "./middleware/auth";
 
 dbConnect();
 
