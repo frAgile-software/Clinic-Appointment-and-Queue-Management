6d87e8e5aa61e46fd22026f810600f41bcbf1262
21Ethan <bothaethan44@gmail.com>
2026-04-18 23:52:58 +0200

    initial routing setup

--- server/src/routes/clinic/getClinic.js
+++ server/src/routes/clinic/getClinic.js
@@ -1,8 +1,34 @@
-// Already under '/api/clinic'
-// For admins and/or clinic staff to use
+// Already under '/clinics'
+// Use filtering parameters that are in the link, i.e. like '.../clinics?example=info' 
 
 
-// Used to pass build checks, replace below
 const express = require("express");
+const Clinic = require("../../database/models/Clinic");
 const router = express.Router();
-module.exports = router;
+
+router.get("/:id", async (req, res) => {
+    try{
+        console.log("1. Incoming Payload: ", req.params);
+        const {id} = req.params;
+
+        //Validation
+        if (!id) {
+            console.log("Fail: Missing clinic ID");
+            return res.status(400).json({ error: "Missing required field" });
+        }
+        //find clinic
+        const clinic = await Clinic.findById(id);
+        if (!clinic) {
+            console.log("Fail: Clinic not found");
+            return res.status(404).json({ error: "Clinic not found" });
+        }
+        console.log("2. Validation Passed");
+        res.status(200).json(clinic);
+    }
+    catch (error) {
+        console.error("Error fetching clinics: ", error);
+        res.status(500).json({ message: "Server error" });
+    }
+    
+});
+module.exports = router;    
