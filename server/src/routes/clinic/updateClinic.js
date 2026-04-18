6818f932197782fb67e8190e6a516da123fe3f04
21Ethan <bothaethan44@gmail.com>
2026-04-19 00:08:42 +0200

    initial update

--- server/src/routes/clinic/updateClinic.js
+++ server/src/routes/clinic/updateClinic.js
@@ -3,5 +3,35 @@
 
 // Used to pass build checks, replace below
 const express = require("express");
+const Clinic = require("../../database/models/Clinic");
 const router = express.Router();
+
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateData = req.body;
+         console.log("1. Incoming Payload: ", req.params);
+         console.log("2. Update Data: ", updateData);
+
+        //validation
+        if (!id) {
+            console.log("Fail: Missing clinic ID");
+            return res.status(400).json({ error: "Missing clinic ID" });
+        }
+        console.log("2. Validation Passed");
+        // Check if clinic exists and update
+        const updatedClinic = await Clinic.findByIdAndUpdate(
+            id, 
+            updateData, 
+               
+        );
+        if (!updatedClinic) {
+            console.log("Fail: Clinic not found");
+            return res.status(404).json({ error: "Clinic not found" });
+        }
+    } catch (error) {
+        console.error("Error updating clinic:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 module.exports = router;
