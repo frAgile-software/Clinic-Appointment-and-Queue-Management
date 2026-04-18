ae0075eace6030b608a8ec6ce6f93ec84deea5cf
21Ethan <bothaethan44@gmail.com>
2026-04-18 21:30:36 +0200

    route validation

--- server/src/routes/clinic/listClinics.js
+++ server/src/routes/clinic/listClinics.js
@@ -5,4 +5,37 @@
 // Used to pass build checks, replace below
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+
+router.get("/", async (req, res) => {
+    try{
+        console.log("1. Incoming Payload: ", req.body);
+        const {auth0Id} = req.body;
+
+        //Validation
+        if (!auth0Id) {
+            console.log("Fail: Missing auth0Id");
+            return res.status(400).json({ error: "Missing required field" });
+        }
+        //check if user is a staff 
+        const user = await Staff.findOne({ auth0Id });
+        if (!user) {
+            console.log("Fail: User not found or is not a staff member");
+            return res.status(404).json({ error: "User not found or is not a staff member" });
+        }
+
+        //check if user has a clinic assigned
+        const clinic = await Clinic.findOne({ staff: auth0Id });
+        if (!clinic) {
+            console.log("Fail: No clinic assigned to this staff member");
+            return res.status(404).json({ error: "No clinic assigned to this staff member" });
+        }
+
+        
+
+        console.log("2. Validation Passed");
+        const clinics = await Clinic.find({ staff: req.body.auth0Id });
+
+    
+});
 module.exports = router;
