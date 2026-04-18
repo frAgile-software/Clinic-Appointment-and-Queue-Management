4061c14b3d7ba355d274362470379db00f7af5c6
r-fletcher <fletcher.ryj@gmail.com>
2026-04-19 01:05:22 +0200

    update user route

--- server/src/routes/users/updateUser.js
+++ server/src/routes/users/updateUser.js
@@ -1,8 +1,26 @@
-// Already under '/api/user'
-// Use '/:auth0Id' or similar
-
-
-// Used to pass build checks, replace below
 const express = require("express");
+const User = require("../../database/models/User");
 const router = express.Router();
 module.exports = router;
+
+router.patch("/:auth0Id", async (req,res) => {
+    try {
+        const { auth0Id } = res.params;
+        const user = await User.findOne({auth0Id});
+
+        if (!user) {
+            return res.status(404).json({ message: "User does not exist."});
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            { auth0Id: auth0Id },
+            { $set: req.body },
+            { new: true, runValidators: true }
+        );
+
+        res.status(200).json({ message: "User updated.", user: updatedUser});
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error." });
+    }
+});
