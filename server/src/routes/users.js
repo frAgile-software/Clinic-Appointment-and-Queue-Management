0a841a06a04b27ffa1bff18654eedfa184e03eba
r-fletcher <fletcher.ryj@gmail.com>
2026-04-18 21:55:25 +0200

    Fixed users router name, and naming of subroutes for users

---
+++ server/src/routes/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const registerUser = require("./users/registerUser");
+const getUser = require("./users/getUser");
+const updateUser = require("./users/updateUser");
+
+router.use("/api/users/register", registerUser);
+router.use("/api/users", getUser);
+router.use("/api/users", updateUser);
+
+module.exports = router;
