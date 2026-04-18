7c8099ea711d566b5a92c46776a9859c296632e2
r-fletcher <fletcher.ryj@gmail.com>
2026-04-18 22:55:40 +0200

    users get by auth0Id testing

--- server/src/index.test.js
+++ server/src/index.test.js
@@ -43,7 +43,39 @@ describe('Server Basic Integrity', () => {
     });
 
     describe('User API', () => {
-        
+        describe('GET /api/users/:id', () => {
+
+            beforeEach(() => {
+                jest.clearAllMocks();
+            });
+
+            test('Returns 200 user when found', async () => {
+                User.findOne.mockResolvedValueOnce({
+                    auth0Id: 'auth0|mockUserId123',
+                    name: 'Test',
+                    surname: 'User',
+                    title: 'Ms',
+                    email: 'test@example.com',
+                    role: 'Admin',
+                });
+
+                const res = await request(app).get('/api/users/auth0|mockUserId123');
+
+                expect(res.statusCode).toEqual(200);
+                expect(res.body).toMatchObject({
+                    role: 'Admin',
+                    name: 'Test',
+                });
+            });
+
+            test('Returns 404 if not found', async() => {
+                User.findOne.mockResolvedValueOnce(null);
+
+                const res = await request(app).get('/api/users/auth0|mockUserId123');
+
+                expect(res.statusCode).toEqual(404);
+            })
+        });
     });
 });
 
