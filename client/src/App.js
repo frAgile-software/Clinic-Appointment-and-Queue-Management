94f0a2ff108a58cabbc378124b0a1b82e248d98f
r-fletcher <fletcher.ryj@gmail.com>
2026-04-19 21:01:40 +0200

    Removec unused Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, Navigate} from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import Landing from           "./pages/Landing/Landing"
