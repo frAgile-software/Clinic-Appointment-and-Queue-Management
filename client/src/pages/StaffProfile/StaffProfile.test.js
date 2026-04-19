46253c20f12eb21c8d1372dc953aada6ef919b33
21Ethan <bothaethan44@gmail.com>
2026-04-19 16:55:04 +0200

    fixed profile tests

--- client/src/pages/StaffProfile/StaffProfile.test.js
+++ client/src/pages/StaffProfile/StaffProfile.test.js
@@ -1,17 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; 
 import StaffProfile from './StaffProfile';
 
 test('renders profile header', () => {  
-    render(<StaffProfile />);      
+    // Wrap the component in MemoryRouter
+    render(
+        <MemoryRouter>
+            <StaffProfile />
+        </MemoryRouter>
+    );      
     const headerElement = screen.getByText(/MY PROFILE/i);
     expect(headerElement).toBeInTheDocument();
 });
 
 test('renders assigned clinics', () => {
-    render(<StaffProfile />);
+    render(
+        <MemoryRouter>
+            <StaffProfile />
+        </MemoryRouter>
+    );
     const clinic1 = screen.getByText(/Hayden Medical Clinic/i);
     const clinic2 = screen.getByText(/Forgotten Dreams Dental Clinic/i);
     expect(clinic1).toBeInTheDocument();
     expect(clinic2).toBeInTheDocument();
 });
-
