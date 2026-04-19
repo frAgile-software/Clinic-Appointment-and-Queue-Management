9be45c2c6120f7bb44ccf5fa9e3f95b19adbfc88
21Ethan <bothaethan44@gmail.com>
2026-04-19 16:48:42 +0200

    test stub

--- client/src/pages/StaffProfile/StaffProfile.js
+++ client/src/pages/StaffProfile/StaffProfile.js
@@ -3,7 +3,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
 function StaffProfile({ staffData }) {
-  const { user } = useAuth0();
+  //const { user } = useAuth0();
   const clinics =[ //test stub 
     {
       _id: "stub-123",
