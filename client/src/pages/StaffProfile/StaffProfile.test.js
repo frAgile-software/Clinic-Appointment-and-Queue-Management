d3e20bc15718b3fa06bc8f94a94091149aae73a7
21Ethan <bothaethan44@gmail.com>
2026-04-19 16:57:19 +0200

    reverted back to old test

--- client/src/pages/StaffProfile/StaffProfile.test.js
+++ client/src/pages/StaffProfile/StaffProfile.test.js
@@ -1,24 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; 
 import StaffProfile from './StaffProfile';
 
-test('renders profile header', () => {  
-    // Wrap the component in MemoryRouter
-    render(
-        <MemoryRouter>
-            <StaffProfile />
-        </MemoryRouter>
-    );      
+test('renders profile header', () => { 
+    render(<StaffProfile />); 
     const headerElement = screen.getByText(/MY PROFILE/i);
     expect(headerElement).toBeInTheDocument();
 });
 
 test('renders assigned clinics', () => {
-    render(
-        <MemoryRouter>
-            <StaffProfile />
-        </MemoryRouter>
-    );
+    render(<StaffProfile />);
     const clinic1 = screen.getByText(/Hayden Medical Clinic/i);
     const clinic2 = screen.getByText(/Forgotten Dreams Dental Clinic/i);
     expect(clinic1).toBeInTheDocument();
