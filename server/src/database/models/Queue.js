f7709e241810b13eef1706d48b3f04e244a91159
21Ethan <bothaethan44@gmail.com>
2026-04-23 10:57:56 +0200

    addPatientQueue further developed

--- server/src/database/models/Queue.js
+++ server/src/database/models/Queue.js
@@ -5,8 +5,6 @@ const queueSchema = new Schema({
 
     Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true}, //FK
     Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}, //FK
-    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true}, //FK
-    Staff: {type: Schema.Types.ObjectId, ref: 'User', required: true}, //FK
     BookingDateTime: {type: Date, required: true}, //This also aids FIFO logic
 });
 
