339f92ad69fa03bc407d0216633ccacca2117189
21Ethan <bothaethan44@gmail.com>
2026-04-23 11:16:31 +0200

    added patient back to queue

--- server/src/database/models/Queue.js
+++ server/src/database/models/Queue.js
@@ -5,6 +5,7 @@ const queueSchema = new Schema({
 
     Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true}, //FK
     Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}, //FK
+    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true}, //FK
     BookingDateTime: {type: Date, required: true}, //This also aids FIFO logic
 });
 
