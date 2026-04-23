fb88bce02fee960073da2707e256f307d49b4917
r-fletcher <fletcher.ryj@gmail.com>
2026-04-23 21:25:45 +0200

    Created skeleton for queues and appointment routes to go in

---
+++ server/src/routes/appointmentsRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// const funkyFunc = require("./appointments/funcName");
+
+// router.use("/api/appointments/...", funkyFunc);
+
+module.exports = router;
---
+++ server/src/routes/queuesRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// const funkyFunc = require("./queues/funcName");
+
+// router.use("/api/queues/...", funkyFunc);
+// router.use("/queues/...", funkyFunc);
+
+module.exports = router;
