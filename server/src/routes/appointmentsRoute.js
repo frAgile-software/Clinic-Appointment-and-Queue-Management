09b124fe4aeaedcf90280b3a211063d2dbf1bc6b
r-fletcher <fletcher.ryj@gmail.com>
2026-04-25 01:31:23 +0200

    Moved updateAppointment to appointments folder

--- server/src/routes/appointmentsRoute.js
+++ server/src/routes/appointmentsRoute.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const updateAppointment = require("./users/updateAppointment");
-// const funkyFunc = require("./appointments/funcName");
+const updateAppointment = require("./appointments/updateAppointment");
 
-// router.use("/api/appointments/...", funkyFunc);
 router.use("/api/appointments", updateAppointment);
 
 module.exports = router;
