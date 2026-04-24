9ff3ed46d64ff73eecb5d28b1c00f8b31e34506f
Bruno Faria <brunofariav@gmail.com>
2026-04-24 08:58:23 +0200

    moved route to appoinment route file

--- server/src/routes/appointmentsRoute.js
+++ server/src/routes/appointmentsRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const updateAppointment = require("./users/updateAppointment");
 // const funkyFunc = require("./appointments/funcName");
 
 // router.use("/api/appointments/...", funkyFunc);
+router.use("/api/appointments", updateAppointment);
 
 module.exports = router;
