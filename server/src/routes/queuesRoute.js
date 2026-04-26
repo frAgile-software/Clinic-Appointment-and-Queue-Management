df26971f6d5e30ff5e14b79718b5688ba3a74194
Bruno Faria <brunofariav@gmail.com>
2026-04-26 14:03:20 +0200

    removed trailing "/" in queue routes

--- server/src/routes/queuesRoute.js
+++ server/src/routes/queuesRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const addPatientQueue = require("./queues/addPatientQueue");
 const updateQueue = require("./queues/updateQueue");
 
-router.use("/api/queues/", addPatientQueue);
+router.use("/api/queues", addPatientQueue);
 router.use("/api/queues", updateQueue);
-router.use("/queues/", addPatientQueue);
+router.use("/queues", addPatientQueue);
 
 module.exports = router;
