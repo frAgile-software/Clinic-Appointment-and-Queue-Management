d495310ca3e5bbd27b7f7f861d553afc9e883ace
Bruno Faria <brunofariav@gmail.com>
2026-04-26 14:25:51 +0200

    created deleteQueue route and added deleteQueue route to queuesRoute.js

--- server/src/routes/queuesRoute.js
+++ server/src/routes/queuesRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const addPatientQueue = require("./queues/addPatientQueue");
+const deleteQueue = require("./queues/deleteQueue");
 
 router.use("/api/queues/", addPatientQueue);
+router.use("/api/queues", deleteQueue);
 router.use("/queues/", addPatientQueue);
+
 
 module.exports = router;
