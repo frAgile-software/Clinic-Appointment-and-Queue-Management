a7c0d0faa7e7b432b2f8e676e67009b2b8de2db2
21Ethan <bothaethan44@gmail.com>
2026-04-26 18:24:49 +0200

    requireAuth

--- server/src/routes/appointmentsRoute.js
+++ server/src/routes/appointmentsRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const {requireAuth} = require("../middleware/auth");
 
 const cancelAppointment = require("./appointments/cancelAppointment");
 
-router.use("/api/appointments/", cancelAppointment);
+router.use("/api/appointments/", requireAuth, cancelAppointment);
 
 module.exports = router;
--- server/src/routes/queuesRoute.js
+++ server/src/routes/queuesRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const {requireAuth} = require("../middleware/auth");
 
 const addPatientQueue = require("./queues/addPatientQueue");
 
-router.use("/api/queues/", addPatientQueue);
+router.use("/api/queues/", requireAuth, addPatientQueue);
 router.use("/queues/", addPatientQueue);
 
 module.exports = router;
