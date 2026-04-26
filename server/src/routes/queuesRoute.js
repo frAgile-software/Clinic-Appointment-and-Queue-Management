e36536818c9f728aa4a901442dab94d6138746d6
HaydnRad <haydn.r.cooke@gmail.com>
2026-04-26 20:52:17 +0200

    fix: actually using the route

--- server/src/routes/queuesRoute.js
+++ server/src/routes/queuesRoute.js
@@ -4,9 +4,11 @@ const { requireAuth } = require('../middleware/auth');
 
 const addPatientQueue = require("./queues/addPatientQueue");
 const updateQueue = require("./queues/updateQueue");
+const getQueue = require("./queues/getQueue");
 
 router.use("/api/queues/", requireAuth, addPatientQueue);
 router.use("/api/queues", requireAuth, updateQueue);
+router.use("/api/queues", requireAuth, getQueue);
 router.use("/queues", addPatientQueue);
 
 module.exports = router;
