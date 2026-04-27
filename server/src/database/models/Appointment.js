b6b895b7a4c7ca293dd6f98ec7c0a711a9c46a4e
Ryx13 <ryanmuzi13@gmail.com>
2026-04-27 21:16:05 +0200

    changed appointment schema to include more info on reason

--- server/src/database/models/Appointment.js
+++ server/src/database/models/Appointment.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const appointmentSchema = new Schema({
-    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true},            // FK
-    Staff: {type: Schema.Types.ObjectId, ref: 'User', required: true},              // FK
-    Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true},           // FK
-    BookingDateTime: {type: Date, required: true},
-    Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}    // FK - reason changed to speciality 
-    // can add a comment field for patients to add more details?
-}, { timestamps: true });   // has createdAt and updatedAt fields, useful info for patient and staff to have
+    Patient: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    Staff: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    Clinic: { type: Schema.Types.ObjectId, ref: 'Clinic', required: true },
+    Speciality: { type: Schema.Types.ObjectId, ref: "Speciality", required: true },
+    BookingDateTime: { type: Date, required: true }, 
+    ReasonDetails: { type: String, trim: true } // e.g., "6 weeks pregnant", "sharp pain in lower back"
+}, { timestamps: true });
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
 module.exports = Appointment;
