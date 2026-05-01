b579b3d386f3242888a46ba87dcdff74fd867959
r-fletcher <fletcher.ryj@gmail.com>
2026-05-01 16:33:00 +0200

    getQueue sorts by updatedAt

--- server/src/routes/queues/getQueue.js
+++ server/src/routes/queues/getQueue.js
@@ -11,7 +11,7 @@ const getBySpecialities = async (specialityIDs, clinic) => {
     const queue = await (uniqueSpecs.length === 0 ?
         Queue.find({ Clinic: clinic }) :
         Queue.find({ Clinic: clinic, Speciality: { $in: uniqueSpecs } })
-    ).sort({ createdAt: 1 });
+    ).sort({ updatedAt: 1 });
 
     return queue;
 };
