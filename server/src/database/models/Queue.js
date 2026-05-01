c697565a72f1a6708460d483b8215e41f837d7cf
HaydnRad <haydn.r.cooke@gmail.com>
2026-05-01 16:15:51 +0200

    revert: undid schema change

--- server/src/database/models/Queue.js
+++ server/src/database/models/Queue.js
@@ -5,8 +5,9 @@ const queueSchema = new Schema({
 
     Clinic: {type: Schema.Types.ObjectId, ref: 'Clinic', required: true}, //FK
     Speciality: {type: Schema.Types.ObjectId, ref: "Speciality", required: true}, //FK
-    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true} //FK
-}, { timestamps: true });
+    Patient: {type: Schema.Types.ObjectId, ref: 'User', required: true}, //FK
+    BookingDateTime: {type: Date, required: true}, //This also aids FIFO logic
+});
 
 const Queue = mongoose.model('Queue', queueSchema);
 module.exports = Queue;
