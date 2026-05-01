832992ce8c176a7a19323ebb4857f7b60c0add3a
r-fletcher <fletcher.ryj@gmail.com>
2026-05-01 18:53:48 +0200

    Check for staff having speciality correction

--- server/src/routes/queues/addPatientQueue.js
+++ server/src/routes/queues/addPatientQueue.js
@@ -4,6 +4,8 @@ const User = require("../../database/models/User");
 const Clinic = require("../../database/models/Clinic");
 const Queue = require("../../database/models/Queue");
 const Speciality = require("../../database/models/Speciality");
+const Staff = require("../../database/models/Staff");
+const StaffSpeciality = require("../../database/models/StaffSpeciality");
 
 router.post("/", async (req, res) => {
     try {
@@ -13,22 +15,37 @@ router.post("/", async (req, res) => {
         //Find user by auth0ID
          
         const user = await User.findOne({ auth0Id: auth0ID }); 
-        if (!user) return res.status(400).json({ message: "User profile not found." });
+        if (!user) {
+            console.log("User profile not found.");
+            return res.status(400).json({ message: "User profile not found." });
+        }
         
         // Get referenced clinic
         const clinic = await Clinic.findOne({ _id: clinicID });
-        if (!clinic) 
+        if (!clinic) {
+            console.log("Clinic not found.");
             return res.status(404).json({ message: "Clinic not found" });
+        }
 
         const speciality = await Speciality.findOne({ SpecialityName: specialityName});
-        if (!speciality) 
+        if (!speciality) {
+            console.log("Speciality not found.");
             return res.status(404).json({ message: "Speciality not found."});
+        }
 
         //check if there's a staff member with the speciality in the clinic
-        const staffMember = await User.findOne({ role: "staff", clinic: clinic._id, speciality: speciality._id });
-        if (!staffMember) 
-        return res.status(404).json({ message: "No staff member with specified speciality found in the clinic." });
+        const staff = await Staff.find({ Clinic: clinic._id }).select('_id');
+        const staffIds = staff.map(s => s._id);
 
+        const staffSpeciality = await StaffSpeciality.findOne({
+            Staff: { $in: staffIds },
+            Speciality: speciality._id
+        });
+
+        if (!staffSpeciality) {
+            console.log("No staff member with specified speciality found in the clinic.");
+            return res.status(404).json({ message: "No staff member with specified speciality found in the clinic." });
+        }
 
         console.log("CLINIC FOUND");
         console.log("APPROPRIATE STAFF MEMBER EXISTS");
