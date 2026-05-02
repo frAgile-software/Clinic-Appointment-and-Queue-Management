c495fa4cc2e16a5f7aaa27063ef7ebcbeb41d3ef
Bruno Faria <brunofariav@gmail.com>
2026-05-02 16:51:11 +0200

    added testing file for removingSpeciality

--- server/src/routes/specialityRoutes.js
+++ server/src/routes/specialityRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { requireAuth } = require("../middleware/auth");
 
 const addSpecialityToStaff = require("./specialities/addSpecialityToStaff");
+const removeSpecialityFromStaff = require("./specialities/removeSpecialityFromStaff");
 
 router.use("/api/specialities", requireAuth, addSpecialityToStaff);
+router.use("/api/specialities", requireAuth, removeSpecialityFromStaff);
 
 module.exports = router;
