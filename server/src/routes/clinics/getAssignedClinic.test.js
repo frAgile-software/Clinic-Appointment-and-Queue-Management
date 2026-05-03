66a02d795d1680b798eb10ffa3058376ef66d14f
21Ethan <bothaethan44@gmail.com>
2026-05-03 10:13:15 +0200

    3 passing

--- server/src/routes/clinics/getAssignedClinic.test.js
+++ server/src/routes/clinics/getAssignedClinic.test.js
@@ -84,4 +84,29 @@ it('should return 400 if auth0Id is missing from the query', async () => {
     expect(response.status).toBe(400);
     expect(response.body).toEqual({ error: "Missing auth0Id" });
 });
+
+it('should return 404 if user is not found or not staff', async () => {
+  const mockUser = { _id: 'userId', auth0Id: 'auth0|123', role: 'NotAStaff' };
+    const mockStaffRecord = { Clinic: 'clinicId1' };
+    const mockClinic = { _id: 'clinicId1', practiceName: 'Clinic 1' };
+
+
+    User.findOne.mockResolvedValue(mockUser);
+
+ 
+    Staff.findOne.mockResolvedValue(mockStaffRecord);
+
+   
+    Clinic.findById.mockResolvedValue(mockClinic);
+
+    const response = await request(app)
+      .get('/?auth0Id=auth0|123');
+
+ 
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ error: "Staff user not found" });
+    
+
+    expect(User.findOne).toHaveBeenCalledWith({ auth0Id: 'auth0|123' });
+});
 });
