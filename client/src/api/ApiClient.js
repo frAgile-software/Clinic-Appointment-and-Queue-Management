d208cf7349a62a1c53cb316097336bf3aa60a81e
r-fletcher <fletcher.ryj@gmail.com>
2026-05-06 20:57:08 +0200

    ApiClient that communicates with server/api

---
+++ client/src/api/ApiClient.js
@@ -0,0 +1,53 @@
+class ApiClient {
+    #baseUrl;
+    #getToken;
+
+    constructor(baseUrl, getToken = null) {
+        this.#baseUrl = baseUrl;
+        this.#getToken = getToken;
+    }
+
+    async #getHeaders() {
+        const headers = { 'Content-Type': 'application/json' };
+        if (this.#getToken) {
+            const token = await this.#getToken();
+            headers.Authorization = `Bearer ${token}`;
+        }
+        return headers;
+    }
+
+    async #request(method, path, { params, body } = {} ) {
+        const url = new URL(`${this.#baseUrl}${path}`);
+
+        // add query params to request
+        if (params) {
+            Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
+        }
+
+        const res = await fetch(url, {
+            method,
+            headers: await this.#getHeaders(),
+            ...(body && { body: JSON.stringify(body)}),
+        });
+
+        if (!res.ok) {
+            const error = await res.json().catch(() => ({}));
+            throw new ApiError(res.status, error.message ?? 'Request failed.');
+        }
+
+        return res.json();
+    }
+
+    get(path, params) { return this.#request('GET', path, { params }); }
+    post(path, body) { return this.#request('POST', path, { body }); }
+    put(path, body) { return this.#request('PUT', path, { body }); }
+    patch(path, body) { return this.#request('PATCH', path, { body }); }
+    delete(path) { return this.#request('DELETE', path); }
+}
+
+class ApiError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
+}
