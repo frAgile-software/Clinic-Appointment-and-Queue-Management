5a327e3bfab3a85f434a463b9f83896c4a3b252f
r-fletcher <fletcher.ryj@gmail.com>
2026-05-06 22:29:49 +0200

    User service

---
+++ client/src/api/UserService.js
@@ -0,0 +1,17 @@
+class UserService extends ResourceService {
+    constructor(publicClient, privateClient) {
+        super(publicClient, privateClient, '/users');
+    }
+
+    register({auth0Id, name, surname, title, email, role}) {
+        return this.priv.post(`${this.basePath}/register`, {auth0Id, name, surname, title, email, role}, null);
+    }
+
+    update(auth0Id, updates) {
+        return this.priv.patch(`${this.basePath}/${auth0Id}`, updates, null);
+    }
+
+    get(auth0Id) {
+        return this.priv.get(`${this.basePath}/${auth0Id}`, null);
+    }
+}
