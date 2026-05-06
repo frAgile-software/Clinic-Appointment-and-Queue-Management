8b5103238fbd7bee166563643e11da404808dfb5
r-fletcher <fletcher.ryj@gmail.com>
2026-05-06 22:04:19 +0200

    full clinic api service (with noted updates)

---
+++ client/src/api/ClinicService.js
@@ -0,0 +1,49 @@
+class ClinicService extends ResourceService {
+    constructor(publicClient, privateClient) {
+        super(publicClient, privateClient, '/clinics');
+    }
+
+    getFilters(queryParams) {
+        return this.pub.get(`${this.basePath}/filters/`, queryParams);
+    }
+
+    filterAll(queryParams) {
+        return this.pub.get(`${this.basePath}/`, queryParams);
+    }
+
+    getById(clinicId) {
+        return this.pub.get(`${this.basePath}/${clinicId}`, null);
+    }
+
+    // uses listAssignedClinics in server
+    // TODO: should use '/api/clinics/assigned/:staffId'
+    getAssignedClinics(auth0Id) {
+        return this.priv.get(`${this.basePath}/assigned`, {auth0Id});
+    }
+
+    // TODO: currently createClinic on server. should be renamed to linkAdmin or something
+    linkAdmin(auth0Id, clinicId, practiceNumber) {
+        return this.priv.post(`${this.basePath}/`, { auth0Id, clinicId, practiceNumber }, null);
+    }
+
+    // TODO: should be patch, not put (since can update individual fields)
+    updateClinic(clinicId, body) {
+        return this.priv.put(`${this.basePath}/${clinicId}`, body, null);
+    }
+
+    removeStaff(clinicId, staffId) {
+        return this.priv.delete(`${this.basePath}/${clinicId}/staff/${staffId}`, null, null);
+    }
+
+    listStaff(clinicId) {
+        return this.priv.get(`${this.basePath}/${clinicId}/staff`, null);
+    }
+
+    /* TODO: auth0Id shouldn't be passed in request body, 
+        and can be obtained through auth token 
+        `const auth0Id = req.auth.payload.sub;` 
+    */
+    linkStaff(clinicId, {auth0Id, id, email}) { // usage: api.clinics.linkStaff(clinicId, {auth0Id: 'uhh', id: '123' });
+        return this.priv.post(`${this.basePath}/${clinicId}/staff`, {id, email, auth0Id}, null);
+    }
+}
