422af62ee0a9ba347f841f5819cdbd7a5c1e7fd0
r-fletcher <fletcher.ryj@gmail.com>
2026-05-07 15:32:11 +0200

    QueueService methods implementation

--- client/src/api/services/QueueService.js
+++ client/src/api/services/QueueService.js
@@ -3,23 +3,27 @@ class QueueService extends ResourceService {
     super(pub, priv, '/queues'); // e.g. '/clinics'
   }
 
-  getForPatient() {
-    
+  getForPatient(patientAuth0Id) {
+    return this.priv.get(`${this.basePath}/patient/${patientAuth0Id}`, null);
   }
 
   addPatient(clinicId, patientId, specialityName) {
-
+    return this.pub.post(`${this.basePath}/`, {clinicId, specialityName, auth0Id});
   }
 
-  remove() {
-    
+  remove(queueId) {
+    return this.priv.delete(`${this.basePath}/${queueId}`, null, null);
   }
 
-  update() {
-
+  update(queueId, {clinicId, specialityId, patientId}) {
+    return this.priv.put(`${this.basePath}/${queueId}`, {clinicId, specialityId, patientId}, null);
   }
 
   get(clinicId, { auth0Id, userId, specialityIDs }) {
-
+    return this.priv.get(`${this.basePath}/${clinicId}`, {
+      auth0Id, 
+      userId, 
+      specialityIDs: Array.isArray(specialityIDs) ? specialityIDs.join(',') : specialityIDs
+    });
   }
 }
