4e7fb07003d5f902ba2f4055c2928998f25572cc
21Ethan <bothaethan44@gmail.com>
2026-05-07 10:21:16 +0200

    test added

--- client/src/pages/StaffProfile/StaffProfile.test.js
+++ client/src/pages/StaffProfile/StaffProfile.test.js
@@ -1,13 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import StaffProfile from './StaffProfile';
+import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router';
 
-test('renders profile header', () => { 
-    render(
-  <MemoryRouter>
-    <StaffProfile />
-  </MemoryRouter>
-);
-    const headerElement = screen.getByText(/MY PROFILE/i);
-    expect(headerElement).toBeInTheDocument();
+//mock auth
+jest.mock('@auth0/auth0-react', () => ({
+    useAuth0: () => ({
+        isAuthenticated: true,
+        user: { sub: 'auth0|123', email: 'test@example.com' },
+        isLoading: false,
+        loginWithRedirect: jest.fn(),
+    }),
+}));
+
+describe('<StaffProfile />', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <StaffProfile />
+            </MemoryRouter>
+        );
+    });
+    it('should render profile nav', ()=>{
+      const form = screen.getByText(/MY PROFILE/i);
+      expect(form).toBeInTheDocument;
+    });
+    it()
 });
+
