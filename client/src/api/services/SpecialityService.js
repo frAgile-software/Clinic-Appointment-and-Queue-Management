95b9a4e4146644b86bc25ae8d63c0cd97ff3afc3
r-fletcher <fletcher.ryj@gmail.com>
2026-05-08 15:11:17 +0200

    SpecialityService class

---
+++ client/src/api/services/SpecialityService.js
@@ -0,0 +1,19 @@
+class SpecialityService extends ResourceService {
+    constructor(publicClient, privateClient) {
+        super(publicClient, privateClient, '/specialities');
+    }
+
+    // TODO: should accept a list of specialityIds (maybe in body)
+    addToStaff({staffId, specialityId}) {
+        return this.priv.post(`${basePath}/staff/${staffId}/${specialityId}`, null, null);
+    }
+
+    // TODO: maybe should accept a list of specialityIds (maybe in body)
+    removeFromStaff({staffId, specialityId}) {
+        return this.priv.delete(`${this.basePath}/staff/${staffId}/${specialityId}`, null, null);
+    }
+
+    getForStaff(staffId) {
+        return this.pub.get(`${this.basePath}/staff/${staffId}`, null);
+    }
+}
