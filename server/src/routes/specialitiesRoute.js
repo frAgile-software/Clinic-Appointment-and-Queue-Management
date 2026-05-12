d9702179e35e7dccff95e4ff3671d074e2829236
r-fletcher <fletcher.ryj@gmail.com>
2026-05-12 19:02:55 +0200

    get specialities for whole clinic route

--- server/src/routes/specialitiesRoute.js
+++ server/src/routes/specialitiesRoute.js
@@ -5,10 +5,12 @@ const { requireAuth } = require("../middleware/auth");
 const addSpecialityToStaff = require("./specialities/addSpecialityToStaff");
 const removeSpecialityFromStaff = require("./specialities/removeSpecialityFromStaff");
 const getStaffSpecialities = require("./specialities/getStaffSpecialities");
+const getClinicSpecialities = require("./specialities/getClinicSpecialities");
 
 router.use("/api/specialities", requireAuth, addSpecialityToStaff);
 router.use("/api/specialities", requireAuth, removeSpecialityFromStaff);
 router.use("/api/specialities", requireAuth, getStaffSpecialities);
+router.use("/api/specialities", requireAuth, getClinicSpecialities);
 
 router.use("/specialities", getStaffSpecialities);
 
