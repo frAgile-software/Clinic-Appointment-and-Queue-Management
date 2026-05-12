fa6691332203b702b4665bbfb5c90dd728fd7f4e
HaydnRad <haydn.r.cooke@gmail.com>
2026-05-12 18:15:52 +0200

    feat: getting appointment stats

---
+++ server/src/routes/appointments/getAppointmentStats.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Appointment = require('../../database/models/Appointment');
+const Clinic = require('../../database/models/Clinic');
+const Staff = require("../../database/models/Staff");
+const User = require("../../database/models/User");
+
+const DATE_SEARCH_FIELDS = ["createdAt", "updatedAt", "BookingDateTime"];
+
+router.get("/:clinicID/stats", async (req, res) => {
+    try {
+        const { clinicID } = req.params;
+        const {
+            auth0Id,
+            date_search_field = "BookingDateTime",
+            _fromdate,
+            _todate,
+            _order = "asc",
+        } = req.query;
+        const specialityIDs = req.query.specialityIDs ? req.query.specialityIDs.split(",") : [];
+        const statuses = req.query.statuses ? req.query.statuses.split(",") : [];
+
+        // Get referenced clinic
+        const clinic = await Clinic.findById({ id: clinicID });
+        if (!clinic)
+            return res.status(404).json({ message: "Clinic not found." });
+
+        // Check if allowed
+        const sender = await User.findOne({ auth0Id });
+        if (!sender || sender.role != "Admin")
+            return res.status(403).json({ message: "Unauthorized." });
+
+        const senderStaff = await Staff.exists({ Clinic: clinic._id, User: sender._id });
+        if (!senderStaff)
+            return res.status(403).json({ message: "Not authorized." });
+
+        const sortOrder = _order === "asc" ? 1 : -1;
+        const targetField = DATE_SEARCH_FIELDS.includes(date_search_field) ? date_search_field : "BookingDateTime";
+        const startDateParam = !_fromdate ? {} : { $gte: new Date(_fromdate) };
+        const endDateParam = !_todate ? {} : { $lt: new Date(_todate) };
+        const dateRangeParam = !_fromdate && !_todate ? {} : { [targetField]: { ...startDateParam, ...endDateParam } };
+        const statusesParam = statuses.length === 0 ? {} : { Status: { $in: statuses } };
+        const specialitiesParam = specialityIDs.length === 0 ? {} : { Speciality: { $in: specialityIDs } };
+
+        const matchingAppointments = await Appointment.find({
+            Clinic: clinic._id,
+            ...dateRangeParam,
+            ...statusesParam,
+            ...specialitiesParam
+        }, '-Remarks -ReasonDetails')
+            .sort({ [targetField]: sortOrder })
+            .populate([
+                { path: 'Patient', select: 'name' },
+                { path: 'Staff', select: 'name' },
+                { path: 'Speciality', select: 'SpecialityName' }
+            ]);
+
+        res.status(200).json(matchingAppointments);
+
+    } catch (error) {
+        console.log("Error getting appointment stats: ", error)
+        res.status(500).json({ message: "Server error." });
+    };
+});
+
+module.exports = router;
