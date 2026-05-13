93a4c6470d2b9f1c93fd3af7bf192963c400d26f
21Ethan <bothaethan44@gmail.com>
2026-05-13 22:12:26 +0200

    Routes added

---
+++ server/src/routes/notifs/deleteSeen.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Notifs = require("../../database/models/Notifs");
+const mongoose = require("mongoose");
+
+router.delete("/:staffId", async (req, res) => {
+    try {
+        console.log("1. Incoming request:", req.params);
+        const {staffId} = req.params;
+        
+        if (!mongoose.Types.ObjectId.isValid(queueId)) {
+            return res.status(400).json({ error: "Invalid queue ID" });
+        }
+
+        const notifs= await Notifs.findAndDelete({
+            Recipient: staffId,
+            Seen: true
+    });
+
+        if (!notifs) {
+            console.log("No notifications deleted");
+            return res.status(200).json({ message: "No notifications deleted" });
+        }
+        console.log("Seen notifications deleted");
+        res.status(200).json(notifs);
+        
+    } catch (error) {
+        if (error.name === "CastError") {
+            console.error("Invalid ObjectId format:", req.params.id);
+            return res.status(400).json({ error: "Invalid staff ID format" });
+        }
+        console.error("Error fetching notifications:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+module.exports = router;
---
+++ server/src/routes/notifs/getNotifs.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Notifs = require("../../database/models/Notifs");
+
+router.get("/:staffId", async (req, res) => {
+    try {
+        console.log("1. Incoming request:", req.params);
+        const { staffId} = req.params;
+        // Database lookup 
+        const notifs= await Notifs.findById(staffId);
+
+        //No notifications
+        if (!notifs) {
+            console.log("No notifications for user id:", id);
+            return res.status(200).json({ message: "No notifications to show" });
+        }
+        console.log("Notifications found");
+        res.status(200).json(notifs);
+
+    } catch (error) {
+        if (error.name === "CastError") {
+            console.error("Invalid ObjectId format:", req.params.id);
+            return res.status(400).json({ error: "Invalid staff ID format" });
+        }
+        console.error("Error fetching notifications:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+module.exports = router;
