de212994d2657cef8367a0191aaeb689cd5090e4
21Ethan <bothaethan44@gmail.com>
2026-05-14 18:23:35 +0200

    delete seen for all users

--- server/src/routes/notifs/deleteSeen.js
+++ server/src/routes/notifs/deleteSeen.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const Notifs = require("../../database/models/Notifs");
 const mongoose = require("mongoose");
 
-router.delete("/:staffId", async (req, res) => {
+router.delete("/:userId", async (req, res) => {
     try {
         console.log("1. Incoming request:", req.params);
-        const {staffId} = req.params;
+        const {userId} = req.params;
         
-        if (!mongoose.Types.ObjectId.isValid(staffId)) {
-            return res.status(400).json({ error: "Invalid queue ID" });
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({ error: "Invalid user ID" });
         }
 
         const notifs= await Notifs.deleteMany({
-            Recipient: staffId,
+            Recipient: userId,
             Seen: true
     });
 
@@ -26,8 +26,8 @@ router.delete("/:staffId", async (req, res) => {
         
     } catch (error) {
         if (error.name === "CastError") {
-            console.error("Invalid ObjectId format:", req.params.id);
-            return res.status(400).json({ error: "Invalid staff ID format" });
+            console.error("Invalid ObjectId format:", req.params);
+            return res.status(400).json({ error: "Invalid user ID format" });
         }
         console.error("Error fetching notifications:", error);
         res.status(500).json({ message: "Server error" });
