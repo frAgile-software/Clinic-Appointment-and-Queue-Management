b9722a6f29ffa177f635c9f4605d1aa4607d2d38
21Ethan <bothaethan44@gmail.com>
2026-05-14 22:40:39 +0200

    Bare minimum testing

--- server/src/routes/notifs/deleteSeen.js
+++ server/src/routes/notifs/deleteSeen.js
@@ -5,26 +5,17 @@ const mongoose = require("mongoose");
 
 router.delete("/:userId", async (req, res) => {
     try {
-        console.log("1. Incoming request:", req.params);
+        console.log(" Incoming request:", req.params);
         const {userId} = req.params;
-        
-        if (!mongoose.Types.ObjectId.isValid(userId)) {
-            return res.status(400).json({ error: "Invalid user ID" });
-        }
 
         const result = await Notif.deleteMany({
             Recipient: userId,
             Seen: true
     });
 
-    if (result.deletedCount===0) {
-        console.log("No notifications deleted");
-        return res.status(200).json(result);
-    }
-    else{
         console.log("Seen notifications deleted");
         res.status(200).json(result);
-    }
+
     } catch (error) {
         console.error("Error fetching notifications:", error);
         res.status(500).json({ error: "Server error" });
