be8b5dc958217931d924fea7abe68745da2eb239
21Ethan <bothaethan44@gmail.com>
2026-05-14 18:22:24 +0200

    get notifs for all users now

--- server/src/routes/notifs/getNotifs.js
+++ server/src/routes/notifs/getNotifs.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Notifs = require("../../database/models/Notifs");
 
-router.get("/:staffId", async (req, res) => {
+router.get("/:userId", async (req, res) => {
     try {
         console.log("1. Incoming request:", req.params);
-        const { staffId} = req.params;
+        const { userId} = req.params;
         // Database lookup 
-        const notifs= await Notifs.find(staffId);
+        const notifs= await Notifs.find(userId);
 
         //No notifications
         if (!notifs) {
-            console.log("No notifications for user id:", staffId);
+            console.log("No notifications for user id:", userId);
             return res.status(200).json({ message: "No notifications to show" });
         }
         console.log("Notifications found");
@@ -20,7 +20,7 @@ router.get("/:staffId", async (req, res) => {
     } catch (error) {
         if (error.name === "CastError") {
             console.error("Invalid ObjectId format:", req.params);
-            return res.status(400).json({ error: "Invalid staff ID format" });
+            return res.status(400).json({ error: "Invalid user ID format" });
         }
         console.error("Error fetching notifications:", error);
         res.status(500).json({ message: "Server error" });
