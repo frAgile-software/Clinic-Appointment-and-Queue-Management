46194995749a1f077a18d6998474659c2d81c264
Suran <suransaha2006@gmail.com>
2026-05-18 13:05:35 +0200

    added schema for offdays

---
+++ server/src/database/models/OffDays.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const offDaysSchema = new Schema({
+  staff_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Staff",
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+});
+
+const OffDays = mongoose.model("OffDays", offDaysSchema);
+module.exports = OffDays;
