252e9ebca41a5eca182ae03a54fc24519d39f6b4
Suran <suransaha2006@gmail.com>
2026-05-19 09:27:38 +0200

    connected the routes to main route  file

--- server/src/routes/schedulesRoute.js
+++ server/src/routes/schedulesRoute.js
@@ -5,9 +5,13 @@ const { requireAuth } = require('../middleware/auth');
 const updateSchedule = require('./schedules/updateSchedule');
 const getUserSchedule = require('./schedules/getUserSchedule');
 const createSchedule = require('./schedules/createSchedule');
+const editSchedule =     require('./schedules/editSchedule');
+const deleteSchedule =     require('./schedules/deleteSchedule');
 
 router.use("/api/schedules", requireAuth, updateSchedule);
 router.use("/api/schedules", requireAuth, getUserSchedule);
 router.use("/api/schedules", requireAuth, createSchedule);
+router.use("/api/schedules", requireAuth, editSchedule);
+router.use("/api/schedules", requireAuth, deleteSchedule);
 
 module.exports = router;
